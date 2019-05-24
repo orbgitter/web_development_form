19cdbc3b80bd73e005922cf8eb116f02fb56872a
orbrch1 <orrbaruch@se.shenkar.ac.il>
2019-05-24 05:01:25 +0300

    fixed login and placing it in its own folder

--- login/get_params_login.php
+++ login/get_params_login.php
@@ -8,12 +8,12 @@
                 Welcome
                 <?php
                 $un = $_GET["reg_un"];
-                $pw = $_GET["reg_pass";]
+                $pw = $_GET["reg_pass"];
 
                 if ($un == "yonit" && $pw == "111")
-                    echo "<h2>Good morning user" . $un ."</h2>
+                    echo "<h2> Good morning user " . $un . "</h2>";
                 else
-                    echo "<h2>Who are you? you can't get in</h2>
+                    echo "<h2> Who are you? you can't get in </h2>"
 
                 ?>
             </section>
